256f57944b31a9acfa9460d1d10b69d36da257d1
aMahanna <anthony.mahanna@gmail.com>
2021-12-03 22:26:48 -0500

    cleanup image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 cd arangodb
-
-if [ -z "$@" ]
-then
-    echo 'Skipping: No files matching {.cpp, .hpp, .cc, .c, .h} in diff.'
-else
-    echo "-----------"
-    clang-format --version
-    echo "-----------"
-    echo "clang-format -style=file -i $@"
-    echo "-----------"
-    clang-format -style=file -i $@
-    echo "erledigt"
-fi
+echo "-----------"
+clang-format --version
+echo "-----------"
+echo "clang-format -style=file -i $@"
+echo "-----------"
+clang-format -style=file -i $@
+echo "erledigt"
